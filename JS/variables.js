6137c27cdbd4f9b4685a72688c17c257f2353731
Sambasivarao <samba.pendela@gmail.com>
2022-01-11 08:50:30 +0530

    variables

--- JS/variables.js
+++ JS/variables.js
@@ -71,30 +71,103 @@ console.log( obj.sub_one, obj.sub_two, obj.sub_three );
     console.log(i);
     //var : 5
     //let : ReferenceError: i is not defined
+    //var keyword breaks the scope rule
+    //let keyword obeys the scope rule
 */
 
 
-//global variable
+/*
+    //global variable
+    let data = 100;
+
+    {
+        //local variable
+        let data = 200;
+    }
+
+    console.log( data );            
+
+    //var : 200
+    //let : 100
+    //var keyword raised the global polluting issue
+    //let keyword overcomes the global polluting issue
+*/
+
+/*
+    //1) declaration
+    //2) initilization
+    //3) printing
+    console.log( data );
+    let data = 100;
+    //var : undefined
+    //let : ReferenceError: Cannot access 'data' before initialization
+    //var keyword raised the variable hoisting issue
+    //we can overcome variable hoisting by using let keyword
+    //accessing the variables before its declaration and initilization called as variable hoisting
+*/
+
+
+/*
 let data = 100;
-
-
-{
-    //local variable
-    let data = 200;
-    
-}
-
-
-console.log( data );            
-
+let data = 200;
+console.log( data );
+*/
 //var : 200
-//let : 100
+//let : SyntaxError: Identifier 'data' has already been declared
+
+//var keyword allows the duplicate variables
+//let keyword won't allows the duplicate variables
+
+/*
+                var                         let
+    var keyword introduced in          let keyword introduced in 
+    ES1                                 ES6
+
+    var keyword breaks the scope        let keyword obeys the scope
+    rule                                rule
+
+    var keyword allows the duplicate    let keyword won't allows the duplicate
+    variables                           variables
+
+    global polluting issue raised       we can overcome global polluting issue
+    because of var keyword              by using let keyword
+
+    variable hoisting issue raised      we can overcome variable hoisting issue
+    because of var keyword              with let keyword
+
+    var keyword is the global scope     block scoped member
+*/
 
 
 
+//const
+//ES6
+//reinitilization not possible
+
+/*const data = 100;
+data = 200;
+*/
+
+//TypeError: Assignment to constant variable.
+
+const arr1 = [10,20,30,40,50];
+//arr = [];                         //SyntaxError: Identifier 'arr' has already been declared
+//arr = [100,200,300,400,500];        //SyntaxError: Identifier 'arr' has already been declared
 
 
+arr1[0] = 100;
+arr1[5] = 600;
+console.log(arr1);              //[ 100, 20, 30, 40, 50, 600 ]
 
+
+const obj1 = {
+    "key1" : "value1",
+    "key2" : "value2",
+    "key3" : "value3"
+};
+//obj1 = {};                  //TypeError: Assignment to constant variable.
+obj1.key1 = "hello_1";
+console.log( obj1 );            //{ key1: 'hello_1', key2: 'value2', key3: 'value3' }
 
 
 
