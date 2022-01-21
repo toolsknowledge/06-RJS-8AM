88244990bdbcbbe2fa41c54a13de12c28cab1fe6
Sambasivarao <samba.pendela@gmail.com>
2022-01-21 08:55:50 +0530

    code

---
+++ TS/variables.js
@@ -0,0 +1,11 @@
+//string
+//collection of characters called as "string"
+//"" (double quotes) , ''(single quotes) and ``(backtick)
+//``(backtick) operator introduced in "ES6"
+//``(backtick) called as "template literal"
+//``(backtick) used to create the "multiline" strings
+var sub_one = "ReactJS";
+var sub_two = "NodeJS";
+var sub_three = "MongoDB";
+var mern_stack = "".concat(sub_one, "<==>").concat(sub_two, "<==>").concat(sub_three);
+console.log(mern_stack);
---
+++ TS/variables.ts
@@ -0,0 +1,11 @@
+//string
+//collection of characters called as "string"
+//"" (double quotes) , ''(single quotes) and ``(backtick)
+//``(backtick) operator introduced in "ES6"
+//``(backtick) called as "template literal"
+//``(backtick) used to create the "multiline" strings
+var sub_one:string = "ReactJS";
+var sub_two:string = "NodeJS";
+var sub_three:string = "MongoDB";
+var mern_stack:string = `${sub_one}<==>${sub_two}<==>${sub_three}`;
+console.log(mern_stack);
